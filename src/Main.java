966e0dbba193c1a9f0fe973515911117754713f7
Dale Fabic <130316665+aquinofabic@users.noreply.github.com>
2023-10-20 16:25:15 +0100

    Init

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<TelevisionProgram> list = new ArrayList<>();
+
+        while (true) {
+            System.out.println("Name: ");
+            String inputName = scanner.nextLine();
+            if (inputName.isEmpty()) {
+                break;
+            }
+            System.out.println("Duration: ");
+            int inputDuration = scanner.nextInt();
+
+            list.add(new TelevisionProgram(inputName, inputDuration));
+        }
+        System.out.println("Program's maximum duration? ");
+        int maxDuration = scanner.nextInt();
+
+        for (TelevisionProgram e : list) {
+            if (maxDuration >= e.getDuration()) {
+                System.out.println(e);
+            }
+        }
+    }
+}
